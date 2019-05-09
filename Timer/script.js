0259cd66031957dd1c6a1eea29206aebd3168394
IrinZh <irishinskin@gmail.com>
2019-05-10 00:17:55 +0300

    7 урок простое задание

---
+++ Timer/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+  let clock = document.querySelector('.timer');
+
+  function formatTime(x) {
+      if (x < 10) x = '0' + x;
+      return x;
+  }
+  function startTime() {
+      let date = new Date(),
+          hours = date.getHours(),
+          minutes = date.getMinutes(),
+          seconds = date.getSeconds();
+
+      hours = formatTime(hours);
+      minutes = formatTime(minutes);
+      seconds = formatTime(seconds);
+
+      clock.innerHTML = hours + ":" + minutes + ":" + seconds;
+  }
+  setInterval(startTime, 500);
+  
+});
+
+
