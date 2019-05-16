a6f38ea413c59b29f9bc11238c74604ac3128771
IrinZh <irishinskin@gmail.com>
2019-05-16 15:04:24 +0300

    переделала 9 задание 1 часть открытие и закрытие модального окно, переменные оставила

---
+++ YOGA-9/Yoga-9-1/js/script.js
@@ -0,0 +1,147 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+	'use strict';
+
+	let tab = document.querySelectorAll('.info-header-tab'),
+		info = document.querySelector('.info-header'),
+		tabContent = document.querySelectorAll('.info-tabcontent');
+
+	function hideTabContent(a)	{
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	}
+
+	hideTabContent(1);
+
+	function showTabContent(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	}
+
+	info.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target && target.classList.contains('info-header-tab')) {
+			for(let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					hideTabContent(0);
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+ ////таймер
+let deadline = 'May 29 2019 07:00:00 GMT+03:00';
+
+function getTimeRemaining(endtime) {
+let t = Date.parse(endtime) - Date.parse(new Date()),
+		seconds = Math.floor((t / 1000) % 60),
+		minutes = Math.floor((t / 1000 / 60) % 60),
+		hours = Math.floor((t / (1000 * 60 * 60)));
+			
+		return {
+			'total' : t,
+			'seconds' : seconds,
+			'minutes' : minutes,
+			'hours' : hours
+			};
+}
+
+function setClock(id, endtime) {
+	let timer = document.getElementById(id),
+		seconds = timer.querySelector('.seconds'),
+		minutes = timer.querySelector('.minutes'),
+		hours = timer.querySelector('.hours'),
+		timeInterval = setInterval(updateClock,1000);
+			
+	function updateClock() {
+		let t = getTimeRemaining(endtime);
+		//добавляем нули и обрезаем если они лишние
+				seconds.textContent = ('0' + t.seconds).slice(-2); 
+				minutes.textContent = ('0' + t.minutes).slice(-2);
+		//часы у нас могут быть 3-х и более значным числом, так что slice(-2) не подходит
+				if (t.hours<10)	{
+				hours.textContent = ('0' + t.hours);
+				}
+				if (t.hours>=10) {
+				hours.textContent = t.hours;	
+				}
+
+
+		
+		if (t.total <= 0) {
+			clearInterval(timeInterval);
+			seconds.textContent = '00';
+			minutes.textContent = '00';
+			hours.textContent = '00';
+		}
+	}
+//убираем первоначальную задержку в 1 секунду
+	updateClock();  
+}
+setClock('timer', deadline);
+});
+
+
+
+//Модальное окно последняя редакция
+//оставила переменные для наглядности
+//самое сложное - именно в добавлении и удалении класса more-splash
+let more = document.querySelector('.more'), //кнопка узнать больше
+			overlay = document.querySelector('.overlay'), //модальное окно
+			close = document.querySelector('.popup-close'), //кнопка закрытие модального окна
+			descBtn = document.querySelectorAll('.description-btn'),// кнопка узнать подробнее, их много
+			isActiveBtn;//кнопка которая была активной при добавлении classList.add('more-splash') 
+			
+	const bindModal = (overlayStatus, overflowStatus, classListMethod, el) => {
+		if(classListMethod == 'add') isActiveBtn = el;
+		if(!el) el = isActiveBtn;
+		overlay.style.display = overlayStatus;
+		el.classList[classListMethod]('more-splash'); //добавляем класс для срабатывания анимации на клик кнопки или убираем  класс кнопки на которую нажимали ранее при клике на закрытие модального
+		document.body.style.overflow = overflowStatus;
+	};
+
+
+	document.body.addEventListener('click', event => {
+		let target = event.target;
+		for (let i = 0; i < descBtn.length; i++) {
+			if(target == more || target == descBtn[i]) bindModal('block', 'hidden', 'add', target);
+			
+			if(target == close) bindModal('none', '', 'remove');
+		}
+		console.log(target);
+		
+});
+
+
+
+ // Модальное окно вариант продвинутый от сонсеннима
+
+/* let overlay = document.querySelector('.overlay'),
+isActiveBtn;
+
+
+const bindModal = (overlayStatus, overflowStatus, classListMethod, el) => {
+if(classListMethod == 'add') isActiveBtn = el;
+if(!el) el = isActiveBtn;
+overlay.style.display = overlayStatus;
+el.classList[classListMethod]('more-splash');
+document.body.style.overflow = overflowStatus;
+};
+
+
+document.body.addEventListener('click', event => {
+let target = event.target;
+
+if(target.classList.contains('more') || target.classList.contains('description-btn')) bindModal('block', 'hidden', 'add', target);
+if(target.classList.contains('popup-close')) bindModal('none', '', 'remove');
+// console.log(target.classList);
+});
+ */
+
+
+
--- YOGA-9/Yoga-9-2/script.js
+++ YOGA-9/Yoga-9-2/script.js
@@ -20,3 +20,46 @@ age.addEventListener('change', function () {
 // });
 
 
+// по тесту к Бонус уроку №2 - https://learn.javascript.ru/quiz/js-basic
+
+/* let a = (1,5 - 1) * 2;
+alert(a); */
+//почему 8?)))
+
+//  console.log(true + false);
+ //почему 1?))
+
+//  console.log(2 && 1 && null && 0 && undefined);
+ //null ответ
+
+
+/*  let arr = [];
+arr[1] = 1;
+arr[3] = 33;
+console.log(arr.length);
+почему 4?)) */
+
+
+/* function F() { return F; }
+
+alert( new F() instanceof F );
+alert( new F() instanceof Function );
+почему false, true. */
+
+
+/* function User() { }
+
+let vasya = new User();
+
+vasya.__proto__.name = "Vasya";
+console.log(vasya.__proto__);
+vasya.__proto__ и User.prototype ответ */
+
+
+
+/* alert( 20e-1['toString'](2) );
+почему то 10, надо посмотреть что такое  20e-1*/
+
+/* let x = 5;
+alert( x++ );
+почему 5? */
