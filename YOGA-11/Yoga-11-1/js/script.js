e37da685e3af06f756bbb0d156ab2ab37d9cd845
IrinZh <irishinskin@gmail.com>
2019-05-17 23:26:24 +0300

    задание к 11 уроку - что-то пошло не так с отправкой формы

---
+++ YOGA-11/Yoga-11-1/js/script.js
@@ -0,0 +1,165 @@
+
+
+window.addEventListener('DOMContentLoaded', () => {   // window.addEventListener('DOMContentLoaded', function() {
+
+	'use strict';
+
+	let tab = document.querySelectorAll('.info-header-tab'),
+		info = document.querySelector('.info-header'),
+		tabContent = document.querySelectorAll('.info-tabcontent');
+
+		const hideTabContent = (a) => {                       //function hideTabContent(a)	{
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+		}
+	};
+
+	hideTabContent(1);
+
+	const showTabContent = (b)	=> {														//function showTabContent(b) {
+		if (tabContent[b].classList.contains('hide')) {
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+		}
+	};
+
+	info.addEventListener('click', (event) => {      //info.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target && target.classList.contains('info-header-tab')) {
+			for(let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					hideTabContent(0);
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+ ////таймер
+let deadline = 'May 29 2019 07:00:00 GMT+03:00';
+
+const getTimeRemaining = (endtime) => {      //function getTimeRemaining(endtime) {
+let t = Date.parse(endtime) - Date.parse(new Date()),
+		seconds = Math.floor((t / 1000) % 60),
+		minutes = Math.floor((t / 1000 / 60) % 60),
+		hours = Math.floor((t / (1000 * 60 * 60)));
+			
+		return {
+			'total' : t,
+			'seconds' : seconds,
+			'minutes' : minutes,
+			'hours' : hours
+			};
+};
+
+const setClock = (id, endtime) => {      // function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+		seconds = timer.querySelector('.seconds'),
+		minutes = timer.querySelector('.minutes'),
+		hours = timer.querySelector('.hours');
+					
+		const updateClock = () => {        // function updateClock() {               
+		let t = getTimeRemaining(endtime);
+		//добавляем нули и обрезаем если они лишние...а еще типа интерполяция
+				seconds.textContent = (`0${t.seconds}`).slice(-2);    //seconds.textContent = ('0' + t.seconds).slice(-2); 
+				minutes.textContent = (`0${t.minutes}`).slice(-2);    //minutes.textContent = ('0' + t.minutes).slice(-2);
+		//часы у нас могут быть 3-х и более значным числом, так что slice(-2) не подходит
+				if (t.hours<10)	{
+				hours.textContent = (`0${t.hours}`);   //hours.textContent = ('0' + t.hours);
+				}
+				if (t.hours>=10) {
+				hours.textContent = t.hours;
+				}
+		
+		if (t.total <= 0) {
+			clearInterval(timeInterval);
+			seconds.textContent = '00';
+			minutes.textContent = '00';
+			hours.textContent = '00';
+		}
+	};
+	let timeInterval = setInterval(updateClock,1000);
+	updateClock();   //убираем первоначальную задержку в 1 секунду
+};
+setClock('timer', deadline);
+
+// Модальное окно от сонсеннима
+
+let overlay = document.querySelector('.overlay'),
+isActiveBtn;
+
+
+const bindModal = (overlayStatus, overflowStatus, classListMethod, el) => {
+if(classListMethod == 'add') isActiveBtn = el;
+if(!el) el = isActiveBtn;
+overlay.style.display = overlayStatus;
+el.classList[classListMethod]('more-splash');
+document.body.style.overflow = overflowStatus;
+};
+
+
+document.body.addEventListener('click', event => {
+let target = event.target;
+
+if(target.classList.contains('more') || target.classList.contains('description-btn')) bindModal('block', 'hidden', 'add', target);
+if(target.classList.contains('popup-close')) bindModal('none', '', 'remove');
+// console.log(target);
+});
+
+//Отправка формы
+let message = {
+	loading: 'Загрузка...',
+	success: 'Спасибо! Скоро мы с Вами свяжемся!',
+	failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+		input = form.getElementsByTagName('input'),
+		statusMessage = document.createElement('div');
+		statusMessage.classList.add('status');
+
+		form.addEventListener('submit', (event) => { //submit вешается на форму, а не на кнопку!!!!
+		event.preventDefault(); //отмена действия браузера по умолчанию при нажатии на кнопку
+		form.appendChild(statusMessage);
+		
+
+		let request = new XMLHttpRequest();
+		request.open('POST','server.php');
+		request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+		// request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+		let formData = new FormData(form);
+		let obj = {};
+          formData.forEach((value, key) => {
+          obj[key] = value;
+		});
+		let json = JSON.stringify(obj);
+		request.send(json);
+		// request.send(formData);
+
+		request.addEventListener('readystatechange', () =>{
+				if (request.readyState < 4) {
+						statusMessage.innerHTML = message.loading;
+				} else if (request.readyState === 4 && request.status == 200) {
+						statusMessage.innerHTML = message.success;
+				} else {
+						statusMessage.innerHTML = message.failure;
+				}
+		});
+		console.log(request.readyState);	 // https://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp
+		console.log(request.status);
+
+		for (let i = 0; i < input.length; i++) {
+				input[i].value = ''; 
+		}
+	});
+
+// валидация номера телефона
+		document.body.addEventListener('input', (event) => {
+			let target = event.target;
+			if (target.getAttribute('type') === 'tel') {
+			target.value = target.value.replace(/[^0-9+]/, '');
+			}
+		});
+	});
